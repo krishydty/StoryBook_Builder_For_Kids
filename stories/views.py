3af28edc9b62a781c55b2143e3c990669b918b88
krishydty <35625095+krishydty@users.noreply.github.com>
2025-02-16 23:28:48 +0530

    Add function-based views for creating a story and viewing story details

--- stories/views.py
+++ stories/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import StoryCreationForm
+from .models import Story
 
-# Create your views here.
+def create_story(request):
+    if request.method == 'POST':
+        form = StoryCreationForm(request.POST)
+        if form.is_valid():
+            story = form.save(commit=False)
+            story.user = request.user  # Ensure user is assigned
+            story.save()
+            return redirect('story_detail', story_id=story.id)
+    else:
+        form = StoryCreationForm()
+    return render(request, 'stories/create_story.html', {'form': form})
+
+def story_detail(request, story_id):
+    story = get_object_or_404(Story, id=story_id)
+    return render(request, 'stories/story_detail.html', {'story': story})
