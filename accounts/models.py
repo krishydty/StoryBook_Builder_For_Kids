c663aa90dbfd75c3d210ed90d574ea8dfc3f2ddb
krishydty <35625095+krishydty@users.noreply.github.com>
2025-02-16 22:32:16 +0530

    Added user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # Inherits username, email, password, first_name, and last_name
+    age = models.PositiveIntegerField(null=True, blank=True)
+    is_parent = models.BooleanField(default=False)
+    profile_picture = models.ImageField(upload_to='profiles/', null=True, blank=True)
+
+    def __str__(self):
+        return self.username
